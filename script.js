27543409179293ef801c6b93e09349c0352bad1c
Santiago Cardoso <Santiagocardoso245@gmail.com>
2024-09-18 11:34:38 -0500

    primero

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const image = document.querySelector('.main-image');
+    image.addEventListener('click', function() {
+        alert('Te llevará a más detalles.');
+    });
+});
